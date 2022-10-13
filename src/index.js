4236f5b4b5aeb0fd40df17cfc27b2000712f680e
NIRUPAM SUR <92023838+Nirupam1111@users.noreply.github.com>
2022-10-13 13:06:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import Cursor from './components/Cursor';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <Cursor />
     <App />
     </BrowserRouter>
   </React.StrictMode>,
